535f67f633d573abd057f08b47d6e6c0fdfc8fe0
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-12-17 12:24:19 +0100

    Modification du script de création de table

--- script/creation.sql
+++ script/creation.sql
@@ -18,13 +18,6 @@ drop table if exists Note;
 
 .print "\n======== Creation des tables ========\n"
 
--- Creation de la table Note
-.print "CREATE TABLE Note"
-create table if not exists Note (
-
-	idnote integer primary key autoincrement,
-	valeur integer not null, check(valeur between 0 and 5)
-);
 
 -- Creation de la table Periode
 .print "CREATE TABLE Periode"
@@ -41,11 +34,11 @@ create table if not exists Periode (
 	check(jour in ("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi",
 		"Dimanche")),
 
-	check(heure_ouverture_matinale between 0 and 23),
-	check(heure_fermeture between 0 and 23)
+	check(heure_ouverture_matinale between 8 and 12),
+	check(heure_fermeture_matinale between 9 and 12)
 
-	check(heure_ouverture_aprem between 0 and 23),
-	check(heure_fermeture_aprem between 0 and 23)
+	check(heure_ouverture_aprem between 12 and 18),
+	check(heure_fermeture_aprem between 13 and 23)
 );
 
 -- Création de la table gps
@@ -71,21 +64,12 @@ create table if not exists Restaurant (
 	web text not null,
 	photo text not null,
 	cout integer not null,
+	note integer not null,
 	type_cuisine text not null,
 	idgps not null unique,
-	check(type_cuisine in ("Classique","Végétarien","Italien","Chinois","Japonais","Fast food"))
+	check(type_cuisine in ("Classique","Végétarien","Italien","Chinois","Japonais","Fast food")),
+	check(note between 0 and 5),
 	foreign key(idgps) references gps(idgps)
-);
-
--- Création de la table Avoir
-.print "CREATE TABLE Avoir"
-create table if not exists Avoir (
-
-	idresto integer,
-	idnote integer,
-	primary key(idresto,idnote),
-	foreign key(idresto) references Restaurant(idresto),
-	foreign key(idnote) references Note(idnote)
 );
 
 
