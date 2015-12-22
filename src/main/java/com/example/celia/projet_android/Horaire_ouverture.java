b614e417c829ce68f1ed7432cf10739ca021749a
celia-Nassima <celia_saidani@yahoo.fr>
2015-12-22 15:26:09 +0100

    methode recuperation horaire: retour(View view) , methode recuperartion heur onItemSelected(...)

--- src/main/java/com/example/celia/projet_android/Horaire_ouverture.java
+++ src/main/java/com/example/celia/projet_android/Horaire_ouverture.java
@@ -1,6 +1,7 @@
 package com.example.celia.projet_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,10 +10,16 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class Horaire_ouverture extends Activity implements AdapterView.OnItemSelectedListener {
     LinearLayout lh;
     CheckBox lundi, mardi, mercredi, jeudi, vendredi, samedi, dimanche;
     Spinner ouvma, ferma, ouvap, ferap;
+    ArrayList <String> tab_jour= new ArrayList <String>();
+    //String tab_horaire[]= new String[14];
+    ArrayList <String> tab_horaire = new ArrayList <String>();
+   // int j=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +37,11 @@ public class Horaire_ouverture extends Activity implements AdapterView.OnItemSel
 
 
         ouvma = (Spinner) findViewById(R.id.spmo);
+
         ferma = (Spinner) findViewById(R.id.spmf);
 
         ouvap = (Spinner) findViewById(R.id.spamo);
+
         ferap = (Spinner) findViewById(R.id.spamf);
 
 
@@ -41,7 +50,9 @@ public class Horaire_ouverture extends Activity implements AdapterView.OnItemSel
 
     public void jours(View view) {
         String jour = null;
+
         lh.setVisibility(View.VISIBLE);
+
 
 
         //sauvgarde dans la bdd  du jour
@@ -81,9 +92,12 @@ public class Horaire_ouverture extends Activity implements AdapterView.OnItemSel
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        //si on clique sur le spiner :
 
-        //sauvgarde dans la bdd
+
+        if(!parent.getItemAtPosition(position).toString().equals("-"))
+        tab_horaire.add(parent.getItemAtPosition(position).toString());
+
+
         Toast.makeText(this, parent.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
     }
 
@@ -97,18 +111,78 @@ public class Horaire_ouverture extends Activity implements AdapterView.OnItemSel
 
 
         lh.setVisibility(view.INVISIBLE);
+        ouvma.setSelection(0);
+        ferma.setSelection(0);
+        ouvap.setSelection(0);
+        ferap.setSelection(0);
+
 
     }
 
 
-    public void retour(View view) {
+    public void retour(View view){
+        int i=0;
+        boolean l=false,ma=false,me=false ,j=false,v=false,s=false,d=false;
+
+        while (i<7){
+            if((lundi.isChecked() )&& (l==false))
+            { tab_jour.add("lundi"); l=true;}
+
+
+            else{
+
+                if((mardi.isChecked())&(ma==false))
+
+                {tab_jour.add("mardi");ma=true;}
+
+                else{
+
+                    if((mercredi.isChecked())&(me==false))
+                    {  tab_jour.add("mercredi"); me=true;}
+
+                    else{
+
+                        if((jeudi.isChecked()) &(j==false))
+                        {tab_jour.add("jeudi"); j=true;}
+
+                        else{
+
+                            if((vendredi.isChecked())&(v==false))
+                            { tab_jour.add("vendredi");v=true;}
+
+                            else{
+
+                                if((samedi.isChecked()) &(s==false))
+                                { tab_jour.add("samedi"); s=true;}
+                                else
+
+                                {  if((dimanche.isChecked()) & (d==false))
+
+                                {   tab_jour.add("dimanche");d=true;}
+
+
+                                }}}}}}
+            i++;
+
+
+
+
+        }
+
+
 
         // renvois de la ligne inserer a l'activité ajouter resto
+        Intent ii= new Intent();
+        ii.putExtra("result-jour",tab_jour);
+        ii.putExtra("result-horaire",tab_horaire);
 
-      /*  Intent ii= new Intent();
-        ii.putExtra("resultat",note.getText().toString());
         setResult(RESULT_OK,ii);
-        finish();*/
+        finish();
+
+
+
+
+
 
 
     }
