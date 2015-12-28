771361a7a4a02378a6e6eab6f1d8a4b37a085556
celia-Nassima <celia_saidani@yahoo.fr>
2015-12-28 11:50:52 +0100

    modification de l'insertion et réalisation de la suppression(sans les horraire)

---
+++ content-provider/RestoBase.java
@@ -0,0 +1,62 @@
+package com.example.celia.projet_provider;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Luxon on 15/12/2015.
+ */
+public class RestoBase extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "projet-base.db";
+    private static int  NO_VERSION = 28;
+
+    private String suppr_req = "drop table if exists Restaurant;";
+
+    private String creation_req =
+            "create table Restaurant (" +
+                    "nom text," +
+                    "adresse text," +
+                    "tel text," +
+                    "web text," +
+                    "note integer," +
+                    "cout integer," +
+                    "photo text," +
+                    "type_cuisine text," +
+                    "latitude double," +
+                    "longitude double," +
+                    "check(type_cuisine in (\"Classique\",\"Végétarien\",\"Italien\",\"Chinois\",\"Japonais\",\"Fast food\")),\n" +
+                    "check(note between 0 and 5)" +
+                    ");";
+    public RestoBase(Context context){
+
+        super(context,DB_NAME,null, NO_VERSION);
+        Log.d("DATABASE_LOG", "...");
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) throws SQLException{
+
+        Log.d("DATABASE_LOG","Creation base");
+        db.execSQL(creation_req);
+        Log.d("DATABASE_LOG","Creation fait");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        if(newVersion > oldVersion)
+        {
+            Log.d("DATABASE_LOG","Update");
+            db.execSQL(suppr_req);
+            Log.d("DATABASE_LOG", "SUPPR fait");
+            db.execSQL(creation_req);
+            Log.d("DATABASE_LOG", "Nouvell création fait");
+        }
+    }
+
+}
